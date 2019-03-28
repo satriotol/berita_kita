6110acceb3e68d1dbbff53cd34781023fe64c06c
satriolinux <satriotol69@gmail.com>
2019-03-28 22:08:07 +0700

    hapus header tidak penting

--- menu_departemen.php
+++ menu_departemen.php
@@ -16,23 +16,6 @@ $index = query('SELECT * FROM departemen WHERE id_dept>0');
     <meta name="author" content="">
     <link rel="icon" href="images/HIMPRO-logo.png">
     <title>HIMPRO TEKNIK KIMIA UNNES</title>
-    <title>HIMPRO TEKNIK KIMIA UNNES</title>
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <!-- Custom styles for this template -->
-    <link href="css/owl.carousel.css" rel="stylesheet">
-    <link href="css/owl.theme.default.min.css" rel="stylesheet">
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <link href="css/baru.css" rel="stylesheet">
-    <!-- Javascript for custom animation -->
-    <script src="js/animate.js"></script>
-    <script src=""></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-
 </head>
 
 <body>
