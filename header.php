1952a24152173cb94b1148760ce71417015b6093
= <indrawandwiprasetyo@gmail.com>
2019-04-29 15:35:27 +0700

    Tab logo HIMPRO

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
+    <link rel="icon" href="./images/favicon.png">
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
