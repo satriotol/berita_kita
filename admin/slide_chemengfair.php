399c4429a6e9b39b061c35b154e94e6dc260f201
satriolinux <satriotol69@gmail.com>
2019-05-28 01:27:59 +0700

    css tekan

--- admin/slide_chemengfair.php
+++ admin/slide_chemengfair.php
@@ -18,3 +18,10 @@
 </body>
 
 </html>
+<script>
+    $(".button").each(function() {
+        if ((window.location.pathname.indexOf($(this).attr('href'))) > -1) {
+            $(this).addClass('active');
+        }
+    });
+</script>
