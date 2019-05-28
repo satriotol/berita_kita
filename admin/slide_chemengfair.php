b3f2ff7542b18cec912871b4e0f02cfc969ee3f7
satriolinux <satriotol69@gmail.com>
2019-05-28 09:52:56 +0700

    slide

--- admin/slide_chemengfair.php
+++ admin/slide_chemengfair.php
@@ -8,10 +8,10 @@
     <h2>Chemengfair Slide</h2>
     <div class="button_all">
         <a class="button">SEMNIAR NASIONAL</a>
-        <a class="button">ISO</a>
+        <a href="list_slide_chem_iso.php" class="button">ISO</a>
         <a href="list_slide_chem_cesa.php" class="button">CESA</a>
         <a href="list_slide_chem_s2c.php" class="button">S2C</a>
-        <a class="button">CEPTION</a>
+        <a href="list_slide_chem_ception.php" class="button">CEPTION</a>
         <a class="button">CHEMENG AWARDS</a>
     </div>
 
